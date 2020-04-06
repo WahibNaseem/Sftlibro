4193ea986f9e15fa06f32e1a303e0d353fd8f4fc
WahibAshraf <Wahib1900@gmail.com>
2020-04-06 23:24:15 +0800

    Sft Calculator

---
+++ CalculatorSft/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorSft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value = "5 + 2 - 3 * 2 / 3 ";
+            do
+            {
+                Console.WriteLine("Enter Maths Expression to calculate");
+                value = Convert.ToString(Console.ReadLine());
+                Console.WriteLine("Answer is :{0} ", sftCalculatorlib.StfCalculator.ExpressionEvaluate(value.ToCharArray()));
+                
+            }
+            while (true);
+        }
+    }
+}
